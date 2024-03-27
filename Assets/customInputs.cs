2015b4269b2644e9814e98f9dff05bb3b63bcca7
Sami Huttula <112397834+Samburker@users.noreply.github.com>
2024-03-27 10:40:43 +0200

    custom input and ui stuff

---
+++ Assets/customInputs.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customInputs : MonoBehaviour
+{
+
+    public void OnMenu() => Debug.Log("Menu");
+    
+}
